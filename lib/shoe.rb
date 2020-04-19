c3c40c97094b1ee1979b2cc91ffcfdea68a9c234
Jiyon FL <jiyonj@hotmail.com>
2020-04-19 18:34:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,5 +5,10 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
 end# Make your shoe class here!
